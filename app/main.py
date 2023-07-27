1ff5fd362c75c5043963eba523cf1c190b608830
ddcroft73 <gen.disarray73@outlook.com>
2023-07-26 20:03:31 -0400

    Major changes to the structure of the project. It just makes better sense this way.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-#from app.api.v1.routers.users import router as users_router
-#from app.api.v1.routers.auth import router as auth_router
 from app.api.v1.api import api_router
 from core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
-
-#app.include_router(users_router, prefix=settings.API_V1_STR, tags=["Users"])
-#app.include_router(auth_router, prefix=settings.API_V1_STR, tags=["Auth"])
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
