86253c1b4803d1e921d7e1e3273bd5ad0a17b872
ddcroft73 <gen.disarray73@outlook.com>
2023-07-27 20:32:43 -0400

    Going all in on the user madels and database code... Fingers crossed!

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,19 @@
 from sqlalchemy import Column, Integer, String, Float
 
 from app.database.base_class import Base
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.database.base_class import Base
 
 
 class User(Base):
     id = Column(Integer, primary_key=True, index=True)
-    uuid = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String, nullable=False)
-    password = Column(Float, nullable=False)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean(), default=True)
+    is_superuser = Column(Boolean(), default=False)
+    items = relationship("Item", back_populates="owner")
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,7 +1,8 @@
+
+'''
 from typing import Optional
 
 from pydantic import BaseModel
-
 
 class UserBase(BaseModel):
     id: Optional[int]
@@ -24,3 +25,44 @@ class UserUpdate(UserBase):
 class UserResponse(UserBase):
     class Config:
         orm_mode = True
+
+'''
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+# Shared properties
+class UserBase(BaseModel):
+    email: Optional[EmailStr] = None
+    is_active: Optional[bool] = True
+    is_superuser: bool = False
+    full_name: Optional[str] = None
+
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    email: EmailStr
+    password: str
+
+
+# Properties to receive via API on update
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class UserInDBBase(UserBase):
+    id: Optional[int] = None
+
+    class Config:
+        orm_mode = True
+
+
+# Additional properties to return via API
+class User(UserInDBBase):
+    pass
+
+
+# Additional properties stored in DB
+class UserInDB(UserInDBBase):
+    hashed_password: str
