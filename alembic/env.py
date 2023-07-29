2b30ee37f90fd19228abdd9a554ead7c70c7377a
ddcroft73 <gen.disarray73@outlook.com>
2023-07-28 20:01:49 -0400

    More database work. working with alembic env.py, etc...

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,13 @@
 from logging.config import fileConfig
-
+import os
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 
+
+from app.database.base import Base  # noqa
+from app.core.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +21,22 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+
+target_metadata = Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def get_url():
+    #user = os.getenv("POSTGRES_USER", "postgres")
+    #password = os.getenv("POSTGRES_PASSWORD", "")
+    #server = os.getenv("POSTGRES_SERVER", "db")
+    #db = os.getenv("POSTGRES_DB", "app")
+    return settings.SQLALCHEMY_DATABASE_URI#f"postgresql://{user}:{password}@{server}/{db}"
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,13 +56,10 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
+    url = settings.SQLALCHEMY_DATABASE_URI
+
+    context.configure(url=url, target_metadata=target_metadata, literal_binds=True,
+                      dialect_opts={"paramstyle": "named"}, compare_type=True)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -57,12 +72,17 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = settings.SQLALCHEMY_DATABASE_URI
+    connectable = engine_from_config(configuration, prefix="sqlalchemy.", poolclass=pool.NullPool)
+    
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata, compare_type=True)
 
+        with context.begin_transaction():
+            context.run_migrations()
+
+    '''
     with connectable.connect() as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata
@@ -70,7 +90,7 @@ def run_migrations_online() -> None:
 
         with context.begin_transaction():
             context.run_migrations()
-
+'''
 
 if context.is_offline_mode():
     run_migrations_offline()
--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,8 @@
 import os
 from pydantic import BaseSettings
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
@@ -10,8 +13,44 @@ class Settings(BaseSettings):
 
     CELERY_BROKER_URL: str ="redis://redis:6379/0"
     CELERY_RESULT_BACKEND: str ="redis://redis:6379/0"
+    
+    '''
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ['*']
 
-    #SQLALCHEMY_DATABASE_URI: str = "postgresql://localhost:5432/fastapi_db"
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+    SENTRY_DSN: Optional[HttpUrl] = None
+
+    @validator("SENTRY_DSN", pre=True)
+    def sentry_dsn_can_be_blank(cls, v: str) -> Optional[str]:
+        if len(v) == 0:
+            return None
+        return v
+
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+'''
+    SQLALCHEMY_DATABASE_URI: str = "postgresql://localhost:5432/dms_User_db"
 
     class Config:
         env_file = "../.env"
