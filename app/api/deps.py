14fed6c10056bd01b4665fb4e09fa157b7cf6dff
ddcroft73 <gen.disarray73@outlook.com>
2023-07-28 20:21:39 -0400

    Added deps.py.

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,39 +1,61 @@
-from fastapi.security import OAuth2PasswordBearer
+from typing import Generator
+
 from fastapi import Depends, HTTPException, status
-from jose import jwt, JWTError
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from pydantic import ValidationError
 from sqlalchemy.orm import Session
-from app.api.schemas.token import TokenData
-from app.crud import crud_user
-from app.api.auth.auth import SECRET_KEY, ALGORITHM
-from app.database.setup import SessionLocal
+
+from app import crud, models, schemas
+from app.core import security
+from app.core.config import settings
+from app.database.session import SessionLocal
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+)
 
 
-def get_db():
-    db = SessionLocal()
+def get_db() -> Generator:
     try:
+        db = SessionLocal()
         yield db
     finally:
         db.close()
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid authentication credentials",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
+def get_current_user(
+    db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
+) -> models.User:
     try:
-        payload = jwt.decode(token, key=SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = crud_user.get_user_by_username(db=db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = schemas.TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = crud.user.get(db, id=token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
     return user
+
+
+def get_current_active_user(
+    current_user: models.User = Depends(get_current_user),
+) -> models.User:
+    if not crud.user.is_active(current_user):
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+def get_current_active_superuser(
+    current_user: models.User = Depends(get_current_user),
+) -> models.User:
+    if not crud.user.is_superuser(current_user):
+        raise HTTPException(
+            status_code=400, detail="The user doesn't have enough privileges"
+        )
+    return current_user
