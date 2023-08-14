8a6dda5c43118745df68d23c53f39c61c6287db0
ddcroft73 <gen.disarray73@outlook.com>
2023-08-14 18:41:03 -0400

    Updated test_routes.py

--- app/sandbox/test_routes.py
+++ app/sandbox/test_routes.py
@@ -1,4 +1,4 @@
-#from file_handler import filesys
+from file_handler import filesys
 import requests
 import json
 from pydantic import BaseModel
@@ -7,15 +7,52 @@ BASE_URL: str = 'http://web:8000/api/v1/' # Must use this address from within co
 
 #BASE_URL: str = 'http://localhost:8015/api/v1/'
 
-class UpdateUser(BaseModel):
+class CreateUser(BaseModel):
+    email: str = None
+    password: str = None
+    full_name: str = None
+    #phone_number: str = None
+   # is_verified: bool = True
+    #account_locked: bool = False
+    #failed_attempts: int = 0
+
+new_user =  CreateUser(
+    email="user@Email.com",
+    password="super.secret.password",
+    full_name="Valued User num"
+)   
+
+
+class UpdateUserMe(BaseModel):
     email: str = None
     password: str = None
     full_name: str = None
     phone_number: str = None
     is_verified: bool = True
     account_locked: bool = False
-    failed_attempts: int = 0
-    
+    failed_attempts: int = 0    
+
+#
+# Update Current user:
+#
+userUp = UpdateUserMe(
+    full_name= "John P. Doefitch, Jr.",
+    email=None,
+    password=None,
+    phone_number='2347894567',
+    account_locked=False,
+    failed_attempts=0
+)
+
+class UpdateUserID(BaseModel):
+    email: str = None
+    password: str = None
+    full_name: str = None
+    phone_number: str = None
+    is_verified: bool = True 
+    account_locked: bool = False 
+    failed_attempts: int = 0    
+
 
 #Login and get token
 def login(user_id: str, password: str, verbose: bool=True):
@@ -36,6 +73,35 @@ def login(user_id: str, password: str, verbose: bool=True):
 
     return access_token
 
+def update_user(user_id: int):
+    url = f"{BASE_URL}users/{user_id}"
+    headers = {
+        'Authorization': f'Bearer {login(user_id="ddc.dev.python@gmail.com", password="password.for.debugging", verbose=False)}',
+        'Content-Type': 'application/json'
+    }
+
+    update_user = UpdateUserID(
+         full_name="New User Full Name"         
+    )
+    payload = {
+        'full_name': "Ordinary User",
+        'email': "johnDoe@jdoe.com"
+    }
+    response = requests.put(url, headers=headers, json=payload)
+
+    return response.json()
+
+
+def get_all_users():
+    url = f"{BASE_URL}users"
+
+    headers = {
+        'Authorization': f'Bearer {login(user_id="ddc.dev.python@gmail.com", password="password.for.debugging", verbose=False)}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(url, headers=headers)
+
+    return response.json()
 
 def get_me():
     url = f"{BASE_URL}users/me"
@@ -48,8 +114,7 @@ def get_me():
 
     return response.json()
 
-
-def update_me(user: User) -> dict:
+def update_me(user: UpdateUserMe) -> dict:
     url = f"{BASE_URL}users/me"
     headers = {
         'Authorization': f'Bearer {login(user_id="ddc.dev.python@gmail.com", password="password.for.debugging", verbose=False)}',
@@ -58,16 +123,20 @@ def update_me(user: User) -> dict:
     response = requests.put(url, headers=headers, json=user.dict())  
     return response.json()
 
-#
-# Update Current user:
-#
-user = UpdateUser(
-    full_name= "John P. Doefitch, Jr.",
-    email=None,
-    password=None,
-    phone_number='2347894567',
-    account_locked=False,
-    failed_attempts=2
-)
 
-print(update_me(user))
+def create_normal_user(new_user: CreateUser):
+    url = f"{BASE_URL}users"
+    response = requests.post(url, json=new_user.dict())
+    if response.status_code == 200:
+        print("User registered successfully!")
+        print(response.json())
+    else:
+        print(f"Failed to register user. Status code: {response.status_code}")
+        print(response.text)
+
+
+#print(update_me(userUp))
+#create_normal_user(new_user)
+#print(get_all_users())
+print(update_user(2))
+#print(get_me())
